bd6b0aec5eacfdab7de7f6e0ce0f052a28b3d616
Tim Hutchinson <t.hutchinson@utah.edu>
2016-11-22 14:28:33 -0700

    bpt updates; setcoverstacks updates; added bpt plot

--- python/elg_templates/bpt.py
+++ python/elg_templates/bpt.py
@@ -1,5 +1,5 @@
 from os.path import join, basename
-from os imort environ
+from os import environ
 from sys import stderr
 
 import numpy as np
@@ -7,6 +7,7 @@ from SetCoverPy import setcover, mathutils
 from matplotlib import pyplot as plt
 import seaborn as sns
 from astropy.io import fits
+from scipy.optimize import curve_fit
 
 from redmonster.datamgr.io2 import read_ndArch
 
@@ -19,6 +20,11 @@ rmver = 'v1_1_0'
 rmdir = join( environ['REDMONSTER_SPECTRO_REDUX'], environ['RUN2D'],
              '%s' % rmver)
 
+residuals = []
+zs = []
+x = []
+y = []
+
 # loop over all plates
 for plate in plates.keys():
     # open redmonster file
@@ -28,7 +34,40 @@ for plate in plates.keys():
     idlpath = join( environ['BOSS_SPECTRO_REDUX'], environ['RUN2D'],
                    '%s' % plate, 'spPlate-%s-%s.fits' % (plate, plates[plate]) )
     hduidl = fits.open(idlpath)
+    
+    # wavelength solution
+    wave = 10**(hduidl[0].header['COEFF0'] + hduidl[0].header['COEFF1'] *
+                np.arange(hduidl[0].header['NAXIS1']))
 
     # loop over fibers
     for i,spec in enumerate(hduidl[0].data):
-        
+        if hdu[1].data.ZWARNING[i] == 0:
+            if hdu[1].data.CLASS1[i] == 'ssp_galaxy_glob':
+                residual = spec - hdu[2].data[i,0]
+                this_wave = wave / (1 + hdu[1].data.Z1[i])
+                nii = np.abs(wave - 6548.86).argmin()
+                ha = np.abs(wave - 6564.61).argmin()
+                oiii = np.abs(wave - 5008.24).argmin()
+                hb = np.abs(wave - 4862.68).argmin()
+                x.append( np.log10( np.sum( residual[nii-3:nii+4] ) /\
+                                  np.sum( residual[ha-3:ha+4] ) ) )
+                y.append( np.log10( np.sum( residual[oiii-3:oiii+4] ) /\
+                                  np.sum( residual[hb-3:hb+4] ) ) )
+
+
+
+
+
+def gaussian(A, sigma, lambda0, x):
+    return A * np.exp(-(x-lambda0)**2/sigma**2)
+
+
+
+
+
+
+
+
+
+
+
--- python/elg_templates/setcoverstacks.py
+++ python/elg_templates/setcoverstacks.py
@@ -3,7 +3,7 @@ from os import environ
 from sys import stderr
 
 import numpy as np
-import SetCoverPy as SCP
+from SetCoverPy import setcover, mathutils
 from matplotlib import pyplot as plt
 import seaborn as sns
 from astropy.io import fits
@@ -17,7 +17,7 @@ plates = {8123:56931}
 rmver = 'v1_1_0'
 
 # chi2 threshold to use in set cover distance matrix
-mindist = 1
+mindist = 0.06
 
 rmdir = join( environ['REDMONSTER_SPECTRO_REDUX'], environ['RUN2D'],
              '%s' % rmver)
@@ -26,6 +26,7 @@ rmdir = join( environ['REDMONSTER_SPECTRO_REDUX'], environ['RUN2D'],
 for plate in plates.keys():
     # list of fibers classified as galaxy with zwarning=0
     fibers = []
+    zs = []
     # open redmonster file
     hdu = fits.open( join( rmdir, '%s' % plate, 'redmonster-%s-%s.fits'
                           % (plate, plates[plate]) ) )
@@ -43,6 +44,7 @@ for plate in plates.keys():
         if hdu[1].data.ZWARNING[i] == 0:
             if hdu[1].data.CLASS1[i] == 'ssp_galaxy_glob':
                 fibers.append(fiber)
+                zs.append(hdu[1].data.Z1[i])
 
     # nfibers x nfibers matrix to hold distances
     distmat = np.zeros( (len(fibers), len(fibers)) )
@@ -54,16 +56,54 @@ for plate in plates.keys():
     # calculate distance from each fiber to every other fiber
     for i,fiber1 in enumerate(fibers):
         for j,fiber2 in enumerate(fibers):
-            stderr.write('\r Total: %s i: %s j:%s' % (len(fibers), i, j) )
+            stderr.write("\rTotal: %s i: %s j:%s" % (len(fibers), i, j) )
+            # convert to rest-frame wavelength
+            wave1 = wave / (1+zs[i])
+            wave2 = wave / (1+zs[j])
+            # crop spectra to only overlapping region
+            if wave1[0] < wave2[0]:
+                low_bound = np.abs(wave1 - wave2[0]).argmin()
+                data1 = hduidl[0].data[fiber1][low_bound:]
+                sigma1 = hduidl[1].data[fiber1][low_bound:]
+                high_bound = np.abs(wave2 - wave1[-1]).argmin()
+                data2 = hduidl[0].data[fiber2][:high_bound+1]
+                sigma2 = hduidl[1].data[fiber2][:high_bound+1]
+            elif wave1[0] > wave2[0]:
+                low_bound = np.abs(wave2 - wave1[0]).argmin()
+                data2 = hduidl[0].data[fiber2][low_bound:]
+                sigma2 = hduidl[1].data[fiber2][low_bound:]
+                high_bound = np.abs(wave1 - wave2[-1]).argmin()
+                data1 = hduidl[0].data[fiber1][:high_bound+1]
+                sigma1 = hduidl[1].data[fiber1][:high_bound+1]
+            else:
+                data1 = hduidl[0].data[fiber1]
+                sigma1 = hduidl[1].data[fiber1]
+                data2 = hduidl[0].data[fiber2]
+                sigma2 = hduidl[1].data[fiber2]
             # convert inverse variance to sigma squared and add in quadrature
-            sigma2 = (1/hduidl[1].data[fiber1]) + (1/hduidl[1].data[fiber2])
-            # calculate chi2 as distance
-            distmat[i][j] = np.sum( (hduidl[0].data[fiber1] *
-                                     hduidl[0].data[fiber2]) / sigma2 )
+            variance = (1/sigma1) + (1/sigma2)
+            # calculate reduced chi2 as distance
+            distmat[i][j] = np.sum( (data1 - data2)**2 / variance ) /\
+                    data1.shape[0]
             #if distmat[i][j] < mindist:
                 #binmat[i][j] = 1
-    print ' '
+    print " "
     binmat = distmat < mindist
+    cost = np.ones(binmat.shape[0])
+
+    g = setcover.SetCover(binmat, cost)
+    g.greedy()
+    #g.SolveSCP()
+
+    # Get the archetype indices
+    iarchetype = np.nonzero(g.s)[0]
+    print "Number of archetypes: %s\n" % iarchetype.shape
+
+    # How many are covered by each archetype?
+    n_rep = np.sum(binmat[:, iarchetype], axis=0)
+    for i,arch in enumerate(iarchetype):
+        print "Archtype #%s represents %s spectra." % (arch, n_rep[i])
+
 
 
 
